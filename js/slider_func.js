3edc9598da41702f3446e7779e867de365bf34fb
José Quiroz <jo_quiroz@outlook.com>
2019-12-25 23:15:51 -0500

    Pausa slider

--- js/slider_func.js
+++ js/slider_func.js
@@ -1,7 +1,7 @@
    $(document).on('ready', function(){
         'use strict';
         $('#layerslider').layerSlider({
-            autoStart: true,
+            autoStart: false,
 			responsive: true,
 			responsiveUnder: 1280,
 			layersContainer: 1170,
@@ -10,5 +10,6 @@
             skinsPath: 'layerslider/skins/'
             // Please make sure that you didn't forget to add a comma to the line endings
             // except the last line!
+            //Regresar el autoStart a True
         });
     });// JavaScript Document
